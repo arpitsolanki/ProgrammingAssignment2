664da527e2d9397b9c656fcc5a7202f262a18a05
arpitsolanki <arpitsolanki14@gmail.com>
2014-12-20 19:21:34 +0530

    submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+#Coursera R Programming Assignment 2, Lexical Scoping
+#Arpit Solanki
 
 
-
-cacheSolve(a)
-
+#Cache matrix creates a list of four functions namely-
+#1. get
+#2. set
+#3. getmatrix
+#4. setmatrix
+#
 makeCacheMatrix <- function(x = matrix()) {
   m<-NULL
   set<-function(y){
@@ -21,10 +22,14 @@ makeCacheMatrix <- function(x = matrix()) {
        getmatrix=getmatrix)
 }
 
+#Cache solve returns a inverse of the matrix inputted 
+#This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+#If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
+
 cacheSolve <- function(x=matrix(), ...) {
   m<-x$getmatrix()
   if(!is.null(m)){
-    message("getting cached data")
+    message("Result already computed, returning cached data")
     return(m)
   }
   matrix<-x$get()
@@ -36,3 +41,4 @@ cacheSolve <- function(x=matrix(), ...) {
 mat<-matrix(data=c(1:4),nrow=2,ncol=2)
 a<-makeCacheMatrix()
 a$set(matrix(1:4,2,2))
+cacheSolve(a)
